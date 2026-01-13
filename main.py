d839bd8c2b177363d0a7d6b350ac93221e4650e7
JTRENZ <167415466+JTRENZ@users.noreply.github.com>
2026-01-13 14:33:36 +0100

    load all highways via api call

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from bundesdata-rag!")
+import requests
 
+url = "https://verkehr.autobahn.de/o/autobahn"
+
+
+def main():
+    r = requests.get(url, timeout=20)
+    r.raise_for_status()
+
+    allroadsdict = r.json()
+    print(allroadsdict)
 
 if __name__ == "__main__":
     main()
